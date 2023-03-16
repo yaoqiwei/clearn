a737e650bb3f9eee0c84ec6d6148d5bb335ae043
yaoqiwei <815482326@qq.com>
2023-03-16 10:24:09 +0800

    main.c

---
+++ begin/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // int dogs;
+  // printf("How many dogs do you have?\n");
+  // scanf("%d", &dogs);
+  // printf("So you have %d dog(s)!\n", dogs);
+  // return 0;
+  int num;
+  num = 1;
+  printf("I'm simply!\n");
+  printf("computer\n");
+  printf("I hava %d computer", num);
+  getchar();
+  return 0;
+}
