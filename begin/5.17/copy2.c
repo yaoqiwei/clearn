3ae8085571bcbfcee2ec32829db1d4fb1d71e95d
yaoqiwei <815482326@qq.com>
2023-05-18 10:38:23 +0800

    5.18

---
+++ begin/5.17/copy2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 40
+#define TARGSIZE 7
+#define LIM 5
+
+char *s_gets(char *st, int n);
+
+int main(void)
+{
+  char qwords[LIM][TARGSIZE];
+  char temp[SIZE];
+  int i = 0;
+  printf("Enter %d words beginning with q:\n", LIM);
+}
